5f2fa82c1ae549c7b8862ca15347926e585522bb
Anubhav Singhal <anubhav.s@ip-192-168-1-32.ap-southeast-1.compute.internal>
2022-08-06 01:54:33 +0530

    Stupid user contract

--- migrations/2_deploy_migrations.js
+++ migrations/2_deploy_migrations.js
@@ -1,5 +1,8 @@
+var Users = artifacts.require("Users")
 var PropertyRegistry = artifacts.require("PropertyRegistry")
 
 module.exports = function(deployer) {
   deployer.deploy(PropertyRegistry);
+  deployer.deploy(Users);
 };
+
